06fd447a4fa7199fceb66fa4a75673170a03a5da
mehr30na <mehr30na@gmail.com>
2017-05-30 14:18:13 +0430

    2/9

---
+++ ionic/src/providers/app-service/app-service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+ Generated class for the AppServiceProvider provider.
+
+ See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+ for more info on providers and Angular 2 DI.
+ */
+
+@Injectable()
+export class AppServiceProvider {
+
+  constructor(public http: Http,) {
+  }
+
+  save(url: string, object) {
+    return this.http.post(url, object)
+      .map(res => res.json());
+  }
+
+
+}
